b8949a066f65ef186868aff877ec8760a6b23c0d
Nick <melancholic8078@gmail.com>
2026-02-23 12:59:04 +0200

    initialized repo

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { databaseInit } from './db/sequelizer/sequelizer.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import cors from 'cors';
+import path from 'path';
+import heroRoutes from './routes/heroRoutes.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundRouteHandler } from './middlewares/notFoundRouteHandler.js';
+
+const app = express();
+
+const PORT = Number(getEnvVar('PORT', 3000));
+
+app.use(express.json());
+app.use(cors());
+
+async function startServer(req, res) {
+  try {
+    await databaseInit();
+    console.log('✅ Database successfully initialized');
+  } catch (err) {
+    console.error('❌ Failed to start the server: Database initialization error.', err);
+  }
+}
+
+// Middleware handlers
+app.use(errorHandler);
+app.use(notFoundRouteHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
