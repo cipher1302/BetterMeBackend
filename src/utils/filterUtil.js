35acfabe90ca8d8de4231da7d816cea2c6489f8e
Ivan Telizhenko <153617015+ivantelizhenko@users.noreply.github.com>
2026-02-28 11:02:11 +0200

    update filters

--- src/utils/filterUtil.js
+++ src/utils/filterUtil.js
@@ -1,5 +1,7 @@
 import { Op } from "sequelize";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+dayjs.extend(utc);
 
 export default function buildFilterQuery(filters) {
   const where = {};
